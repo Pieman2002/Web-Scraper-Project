0b498f28d0054d5b97a618ac0e9ec10fe02518ed
NightShark115 <43865882+NightShark115@users.noreply.github.com>
2020-02-14 11:26:28 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,28 @@
 # Scraper! Go wild 
 # This will consist of the combined code (and documentation) of all parties.
 
+import requests
+import re
+from bs4 import BeautifulSoup
 
+url = "https://katu.com/resources/ftptransfer/katu/traffic/traffic.html"
+page  = requests.get(url)
+
+soup = BeautifulSoup(page.text, "html.parser")
+results = soup.find_all("div", attrs={"class":"cwcReport Oregon"})
+ResultStr = str(results)
+
+
+#records = []
+#for result in results:
+  #alert = result.find('n">').text[0:-7]
+  # lie = result.contents[1][1:-2]
+  # explanation = result.find('a').text[1:-1]
+  # url = result.find('a')['href']
+  #records.append((alert))
+ 
+
+
+print(len(results))
+print(results)
+#print(records)
